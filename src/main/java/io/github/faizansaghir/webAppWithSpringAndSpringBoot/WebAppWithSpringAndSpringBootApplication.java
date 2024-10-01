6a45df7bbb4eb281aa80727d0512b41be2e53cf6
faizansaghir <dbz.rashidsaghir@gmail.com>
2024-10-01 20:38:04 +0530

    Created initial project structure for web application

---
+++ src/main/java/io/github/faizansaghir/webAppWithSpringAndSpringBoot/WebAppWithSpringAndSpringBootApplication.java
@@ -0,0 +1,13 @@
+package io.github.faizansaghir.webAppWithSpringAndSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppWithSpringAndSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppWithSpringAndSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/faizansaghir/webAppWithSpringAndSpringBoot/WebAppWithSpringAndSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.faizansaghir.webAppWithSpringAndSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppWithSpringAndSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
