a77adbb410128d6a402cfb76562f222514ae790e
faizansaghir <dbz.rashidsaghir@gmail.com>
2024-10-01 21:01:26 +0530

    Added code to specify server port and introduced test controller

---
+++ src/main/java/io/github/faizansaghir/webAppWithSpringAndSpringBoot/hello/SayHelloController.java
@@ -0,0 +1,16 @@
+package io.github.faizansaghir.webAppWithSpringAndSpringBoot.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+
+    @RequestMapping("say-hello")
+    @ResponseBody
+    public String sayHello(){
+        return "Hello! How are you doing?";
+    }
+
+}
