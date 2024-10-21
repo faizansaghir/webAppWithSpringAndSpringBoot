c5654d451dabfec454829cfe8e954e73c3778808
faizansaghir <dbz.rashidsaghir@gmail.com>
2024-10-21 07:01:36 +0530

    Added feature to delete todo

--- src/main/java/io/github/faizansaghir/webAppWithSpringAndSpringBoot/todo/TodoService.java
+++ src/main/java/io/github/faizansaghir/webAppWithSpringAndSpringBoot/todo/TodoService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 @Service
 public class TodoService {
@@ -25,5 +26,10 @@ public class TodoService {
     public void addTodo(String username, String description, LocalDate targetDate, boolean done){
         Todo newTodo = new Todo(++todoCount, username, description, targetDate, done);
         todos.add(newTodo);
+    }
+
+    public void deleteTodoById(int id){
+        Predicate<? super Todo> predicate = todo -> todo.getId()==id;
+        todos.removeIf(predicate);
     }
 }
