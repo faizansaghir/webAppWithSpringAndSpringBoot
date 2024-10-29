3abdb947b41b8707fe61380a24a57c635f0a3aa2
faizansaghir <dbz.rashidsaghir@gmail.com>
2024-10-29 22:24:58 +0530

    Updated logic to get username from Spring Security context

--- src/main/java/io/github/faizansaghir/webAppWithSpringAndSpringBoot/login/WelcomeController.java
+++ src/main/java/io/github/faizansaghir/webAppWithSpringAndSpringBoot/login/WelcomeController.java
@@ -2,11 +2,12 @@ package io.github.faizansaghir.webAppWithSpringAndSpringBoot.login;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
@@ -17,8 +18,12 @@ public class WelcomeController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String renderLoginPage(ModelMap model) {
-        model.addAttribute("username", "Default User");
+        model.addAttribute("username", getLoggedUser());
         return "welcome";
     }
 
+    private String getLoggedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getName();
+    }
 }
