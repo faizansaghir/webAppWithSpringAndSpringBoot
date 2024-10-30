9cbfe9bd3931ec2b5b7a186c8974923532aab5f7
faizansaghir <dbz.rashidsaghir@gmail.com>
2024-10-30 22:58:32 +0530

    Disabled csrf and frameOptions in SecurityFilterChain by overriding SecurityFilterChain bean creation logic

--- src/main/java/io/github/faizansaghir/webAppWithSpringAndSpringBoot/security/SpringSecurityConfiguration.java
+++ src/main/java/io/github/faizansaghir/webAppWithSpringAndSpringBoot/security/SpringSecurityConfiguration.java
@@ -2,11 +2,16 @@ package io.github.faizansaghir.webAppWithSpringAndSpringBoot.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
 
@@ -32,5 +37,17 @@ public class SpringSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated()
+        );
+        http.formLogin(Customizer.withDefaults());
+
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+        return http.build();
     }
 }
