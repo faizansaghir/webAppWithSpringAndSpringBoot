9710302ac3214cb81586e76355949c029b92a61b
faizansaghir <dbz.rashidsaghir@gmail.com>
2024-10-30 22:26:57 +0530

    Added new user to web application

--- src/main/java/io/github/faizansaghir/webAppWithSpringAndSpringBoot/security/SpringSecurityConfiguration.java
+++ src/main/java/io/github/faizansaghir/webAppWithSpringAndSpringBoot/security/SpringSecurityConfiguration.java
@@ -14,14 +14,19 @@ import java.util.function.Function;
 public class SpringSecurityConfiguration {
     @Bean
     public InMemoryUserDetailsManager createUserDetailsManager() {
+        UserDetails userDetails1 = createNewUser("Faizan", "pass");
+        UserDetails userDetails2 = createNewUser("Admin", "admin");
+        return new InMemoryUserDetailsManager(userDetails1, userDetails2);
+    }
+
+    private UserDetails createNewUser(String username, String password){
         Function<String, String> passwordEncoder =
                 input -> passwordEncoder().encode(input);
-        UserDetails userDetails = User.builder().passwordEncoder(passwordEncoder)
-                .username("faizan")
-                .password("pass")
+        return User.builder().passwordEncoder(passwordEncoder)
+                .username(username)
+                .password(password)
                 .roles("USER", "ADMIN")
                 .build();
-        return new InMemoryUserDetailsManager(userDetails);
     }
 
     @Bean
